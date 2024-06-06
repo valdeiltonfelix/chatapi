8ae6e4ab0a594438680fefa9b0d8b4684bea7a96
VALDEILTON FELIX <javaibotirama@gmail.com>
2024-06-06 11:53:25 -0300

    Resolvendo problema de cors oringin
    Necessario a ação para corrigir o problema que estava dando quando enviava um requisição ajax com jquery

--- api/index.js
+++ api/index.js
@@ -14,6 +14,12 @@ app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
 
+ app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+ });
+
 app.get('/', (req, res) => {
   res.send('Bem vindo a minha api do chat maxone')
 })
